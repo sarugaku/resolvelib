80b4a06e072b11d3229a462092367d6f893f9612
Tzu-ping Chung <uranusjr@gmail.com>
2018-09-03 01:03:01 +0800

    Remove reporting callbacks added in 0.2.1
    They are not useful at all. Remove them before anyone actually uses them.

--- src/resolvelib/reporters.py
+++ src/resolvelib/reporters.py
@@ -11,20 +11,6 @@ class BaseReporter(object):
         The index is zero-based.
         """
 
-    def adding_requirements(self, requirements):
-        """Called before requirements are being added.
-
-        The requirements are not necessarily new.
-        """
-
-    def adding_candidate(self, candidate):
-        """Called before a candidate is being used to pin a requirement.
-        """
-
-    def replacing_candidate(self, current, replacement):
-        """Called before a candidate is being replaced by another as the pin.
-        """
-
     def ending_round(self, index, state):
         """Called before each round of resolution ends.
 
