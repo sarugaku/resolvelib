a9d1cdd7a26903ff15c49c51edd3cba73803c183
Tzu-ping Chung <uranusjr@gmail.com>
2020-03-27 21:55:01 +0800

    Test for requirement-candidate consistency check

---
+++ tests/test_resolvers.py
@@ -0,0 +1,42 @@
+import pytest
+
+from resolvelib import (
+    AbstractProvider,
+    BaseReporter,
+    InconsistentCandidate,
+    Resolver,
+)
+
+
+def test_candidate_inconsistent_error():
+    requirement = "foo"
+    candidate = "bar"
+
+    class Provider(AbstractProvider):
+        def identify(self, d):
+            assert d is requirement or d is candidate
+            return d
+
+        def get_preference(self, *_):
+            return 0
+
+        def get_dependencies(self, _):
+            return []
+
+        def find_matches(self, r):
+            assert r is requirement
+            return [candidate]
+
+        def is_satisfied_by(self, r, c):
+            assert r is requirement
+            assert c is candidate
+            return False
+
+    resolver = Resolver(Provider(), BaseReporter())
+
+    with pytest.raises(InconsistentCandidate) as ctx:
+        resolver.resolve([requirement])
+
+    assert str(ctx.value) == "Provided candidate 'bar' does not satisfy 'foo'"
+    assert ctx.value.candidate is candidate
+    assert list(ctx.value.criterion.iter_requirement()) == [requirement]
