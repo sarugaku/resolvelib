7f0799030fca496368b59611d8a23a075db3fedc
Paul Moore <p.f.moore@gmail.com>
2020-04-03 15:56:10 +0100

    The irony of noxfile.py not checking itself, then failing lint :-(

--- noxfile.py
+++ noxfile.py
@@ -1,14 +1,16 @@
 import nox
 
+
 @nox.session
 def tests(session):
-    session.install('pytest')
-    session.install('.[test]')
-    session.run('pytest', 'tests')
+    session.install("pytest")
+    session.install(".[test]")
+    session.run("pytest", "tests")
+
 
 @nox.session
 def lint(session):
-    session.install('black')
-    session.install('flake8')
-    session.run('black', '--check', '.')
-    session.run('flake8', 'src', 'tests')
+    session.install("black")
+    session.install("flake8")
+    session.run("black", "--check", ".")
+    session.run("flake8", "src", "tests", "noxfile.py")
