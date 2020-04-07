12d790209ef40ad56dc9453354517fb9037c2fa9
Tzu-ping Chung <uranusjr@gmail.com>
2020-04-07 22:53:41 +0800

    Migrate release process to use Nox

--- noxfile.py
+++ noxfile.py
@@ -1,16 +1,101 @@
+import argparse
+import pathlib
+
 import nox
 
 
-@nox.session
+ROOT = pathlib.Path(__file__).resolve().parent.parent
+
+INIT_PY = ROOT.joinpath("src", "resolvelib", "__init__.py")
+
+
+@nox.session()
 def tests(session):
-    session.install("pytest")
     session.install(".[test]")
     session.run("pytest", "tests")
 
 
-@nox.session
+@nox.session()
 def lint(session):
-    session.install("black")
-    session.install("flake8")
+    session.install(".[lint]")
     session.run("black", "--check", ".")
     session.run("flake8", "src", "tests", "noxfile.py")
+
+
+def _write_package_version(v):
+    lines = []
+
+    version_line = None
+    with INIT_PY.open() as f:
+        for line in f:
+            if line.startswith("__version__ = "):
+                line = version_line = f"__version__ = {repr(str(v))}\n"
+            lines.append(line)
+    if not version_line:
+        raise ValueError("__version__ not found in __init__.py")
+
+    with INIT_PY.open("w", newline="\n") as f:
+        f.write("".join(lines))
+
+
+@nox.session()
+def release(session):
+    session.install(".[release]")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--version",
+        help="Version to release. Empty value uses the value in __init__.py",
+    )
+    parser.add_argument(
+        "--repo",
+        help="Repository to upload to. Empty value disables publish.",
+    )
+    parser.add_argument(
+        "--prebump",
+        help="Version to bump to after release. Empty value disables bump.",
+    )
+    options = parser.parse_args(session.postargs)
+
+    # Make sure the workspace is clean.
+    session.run("git", "diff", "--no-patch", "--exit-code", external=True)
+
+    if options.version:
+        _write_package_version(options.version)
+        session.run("towncrier", "--version", options.version)
+        session.run(
+            "git",
+            "commit",
+            "--all",
+            "--message",
+            f"Release {options.version}",
+            external=True,
+        )
+        session.run(
+            "git",
+            "tag",
+            "--annotate",
+            "--message",
+            f"Version {options.version}",
+            external=True,
+        )
+    else:
+        session.log("Skipping preprocessing since --version is empty")
+
+    if options.repo:
+        session.log(f"Releasing distributions to {options.repo}...")
+        session.run("setl", "publish", "--repository", options.repo)
+    else:
+        session.log(f"Building distributions locally since --repo is empty")
+        session.run(f"setl publish --no-upload")
+
+    if options.prebump:
+        _write_package_version(options.prebump)
+        session.run(
+            "git",
+            "commit",
+            "--all",
+            "--message",
+            f"Prebump to {options.prebump}",
+            external=True,
+        )
