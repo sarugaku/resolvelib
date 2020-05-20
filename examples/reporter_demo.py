d4cfc5bb2b81d6b2e0db85815b676910dd81cbe7
Tzu-ping Chung <uranusjr@gmail.com>
2020-05-20 21:02:02 +0800

    Blackify

--- examples/reporter_demo.py
+++ examples/reporter_demo.py
@@ -27,12 +27,16 @@ third 3.0.0
 
 class Requirement(namedtuple("Requirement", "name specifier")):  # noqa
     def __repr__(self):
-        return "<Requirement({name}{specifier})>".format(name=self.name, specifier=self.specifier)
+        return "<Requirement({name}{specifier})>".format(
+            name=self.name, specifier=self.specifier
+        )
 
 
 class Candidate(namedtuple("Candidate", "name version")):  # noqa
     def __repr__(self):
-        return "<{name}=={version}>".format(name=self.name, version=self.version)
+        return "<{name}=={version}>".format(
+            name=self.name, version=self.version
+        )
 
 
 def splitstrip(s, parts):
@@ -75,15 +79,15 @@ class Provider(resolvelib.AbstractProvider):
         deps = list(
             filter(
                 lambda candidate: self.is_satisfied_by(requirement, candidate),
-                sorted(self.candidates)
+                sorted(self.candidates),
             )
         )
         return deps
 
     def is_satisfied_by(self, requirement, candidate):
         return (
-            candidate.name == requirement.name and
-            candidate.version in requirement.specifier
+            candidate.name == requirement.name
+            and candidate.version in requirement.specifier
         )
 
     def get_dependencies(self, candidate):
@@ -91,7 +95,6 @@ class Provider(resolvelib.AbstractProvider):
 
 
 class Reporter(resolvelib.BaseReporter):
-
     def starting(self):
         print("starting()")
 
@@ -116,6 +119,7 @@ class Reporter(resolvelib.BaseReporter):
 
 if __name__ == "__main__":
     from pprint import pprint
+
     provider = Provider(index.splitlines())
     reporter = Reporter()
     resolver = resolvelib.Resolver(provider, reporter)
