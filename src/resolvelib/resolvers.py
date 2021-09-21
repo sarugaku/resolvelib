0f253d0b99b485af35f78e3db7bd2b5219f0c106
Damian <damian.peter.shaw@gmail.com>
2021-09-21 16:20:44 -0400

    Remove accidental space

--- src/resolvelib/resolvers.py
+++ src/resolvelib/resolvers.py
@@ -234,7 +234,6 @@ class Resolution(object):
             # backtracking looks at this mapping to get the last pin.
             self.state.mapping.pop(name, None)
             self.state.mapping[name] = candidate
-    
             return []
 
         # All candidates tried, nothing works. This criterion is a dead
