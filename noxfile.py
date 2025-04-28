2fa1a485cbfd705126ca8f50eb20bec7072b50da
Tzu-ping Chung <uranusjr@gmail.com>
2025-04-28 16:53:41 +0800

    Drop support for Python 3.7 and 3.8

--- noxfile.py
+++ noxfile.py
@@ -20,7 +20,7 @@ def lint(session):
     session.run("mypy", "src", "tests")
 
 
-@nox.session(python=["3.13", "3.12", "3.11", "3.10", "3.9", "3.8", "3.7"])
+@nox.session(python=["3.13", "3.12", "3.11", "3.10", "3.9"])
 def tests(session):
     session.install(".[test]")
 
